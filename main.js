c7d65b347d543c9945279f5e54c356ca9d99c80c
Ben Wilhelm <ben@screencastify.com>
2020-11-13 15:14:43 -0600

    Add option to return long tag format

--- main.js
+++ main.js
@@ -1,58 +1,64 @@
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 
 // github actions pass inputs as environment variables prefixed with INPUT_ and uppercased
 function getInput(key) {
-    var variable = 'INPUT_'+key;
-    var result = process.env[variable.toUpperCase()];
-    console.log(`Using input for ${key}: ${result}`);
-    return result;
+  var variable = "INPUT_" + key;
+  var result = process.env[variable.toUpperCase()];
+  console.log(`Using input for ${key}: ${result}`);
+  return result;
 }
 
 // rather than npm install @actions/core, output using the console logging syntax
 // see https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
 function setOutput(key, value) {
-    console.log(`::set-output name=${key}::${value}`)
+  console.log(`::set-output name=${key}::${value}`);
 }
 
 try {
-    const include = getInput('include');
-    const exclude = getInput('exclude');
-    const commitIsh = getInput('commit-ish');
-    const skipUnshallow = getInput('skip-unshallow') === 'true';
+  const include = getInput("include");
+  const exclude = getInput("exclude");
+  const commitIsh = getInput("commit-ish");
+  const long = getInput("long") === "true";
+  const skipUnshallow = getInput("skip-unshallow") === "true";
 
-    var includeOption = '';
-    var excludeOption = '';
-    var commitIshOption = '';
+  var includeOption = "";
+  var excludeOption = "";
+  var commitIshOption = "";
+  var abbrevOption = long ? "" : "--abbrev=0";
 
-    if (typeof include === 'string' && include.length > 0) {
-        includeOption = `--match '${include}'`;
+  if (typeof include === "string" && include.length > 0) {
+    includeOption = `--match '${include}'`;
+  }
+
+  if (typeof exclude === "string" && exclude.length > 0) {
+    excludeOption = `--exclude '${exclude}'`;
+  }
+
+  if (typeof commitIsh === "string") {
+    if (commitIsh === "" || commitIsh === "HEAD") {
+      console.warn(
+        'Passing empty string or HEAD to commit-ish will get the "current" tag rather than "previous". For previous tag, try "HEAD~".'
+      );
     }
+    commitIshOption = `'${commitIsh}'`;
+  }
 
-    if (typeof exclude === 'string' && exclude.length > 0) {
-        excludeOption = `--exclude '${exclude}'`;
+  var unshallowCmd = skipUnshallow ? "" : "git fetch --prune --unshallow &&";
+
+  // actions@checkout performs a shallow checkout. Need to unshallow for full tags access.
+  var cmd = `${unshallowCmd} git describe --tags ${abbrevOption} ${includeOption} ${excludeOption} ${commitIshOption}`
+    .replace(/[ ]+/, " ")
+    .trim();
+  console.log(`Executing: ${cmd}`);
+
+  exec(cmd, (err, tag, stderr) => {
+    if (err) {
+      console.error(`Unable to find an earlier tag.\n${stderr}`);
+      return process.exit(1);
     }
-
-    if (typeof commitIsh === 'string') {
-        if (commitIsh === '' || commitIsh === 'HEAD') {
-            console.warn('Passing empty string or HEAD to commit-ish will get the "current" tag rather than "previous". For previous tag, try "HEAD~".');
-        }
-        commitIshOption = `'${commitIsh}'`;
-    }
-
-    var unshallowCmd = skipUnshallow ? '' : 'git fetch --prune --unshallow &&'
-
-    // actions@checkout performs a shallow checkout. Need to unshallow for full tags access.
-    var cmd = `${unshallowCmd} git describe --tags --abbrev=0 ${includeOption} ${excludeOption} ${commitIshOption}`.replace(/[ ]+/, ' ').trim();
-    console.log(`Executing: ${cmd}`);
-
-    exec(cmd, (err, tag, stderr) => {
-        if (err) {
-            console.error(`Unable to find an earlier tag.\n${stderr}`);
-            return process.exit(1);
-        }
-        console.log(`Outputting tag: ${tag.trim()}`)
-        return setOutput('tag', tag.trim());
-    });
+    console.log(`Outputting tag: ${tag.trim()}`);
+    return setOutput("tag", tag.trim());
+  });
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
